ec39894f4d05b9b0af79d8586fedc6713e354914
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2024-07-28 16:57:19 +0530

    Clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import React from 'react';
-
-import BillAnalysisCalculator from './components/PlantLayout.jsx/BillAnalysis/BillAnalysisCalculator';
-import BillAnalysis from './components/PlantLayout.jsx/BillAnalysis/BillAnalysis';
-import PaymentTracking from './components/PlantLayout.jsx/PaymentTrack/PaymentTracking';
-import ReportsDropdown from './components/PlantLayout.jsx/ReportsDropdown';
+import React from "react";
+import ClientPlantTable from "./pages/plants/ClientPlantTable";
 
 const App = () => {
   return (
     <div>
-      {/* <BillAnalysisCalculator/> */}
-      <PaymentTracking/>
-        <ReportsDropdown />
-      <PaymentTracking/>
-      <PaymentTracking/>
-      <BillAnalysis/>
+      <ClientPlantTable />
     </div>
   );
-}
+};
 
 export default App;
